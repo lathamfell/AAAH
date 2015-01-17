5742674db1614fa399e6672eac6095489ab63805
Latham Fell <lathamfell@gmail.com>
2015-01-16 16:19:54 -0800

    added icalendar source code

--- mailbox/handler.py
+++ mailbox/handler.py
@@ -1,21 +1,14 @@
-# database import
 import MySQLdb
-# for sending email
 import smtplib
-# for encoding email
 import email
 import time
 import email.MIMEMultipart
 import email.MIMEBase
 import email.MIMEText
-from email.utils import COMMASPACE, formatdate
-#from email import encoders
-import os
 import datetime as dt
-# icalendar
 import icalendar
-# time zone module
 import pytz
+import random
 
 def main():
   # trival script to tell when main function is called
@@ -27,13 +20,10 @@ def main():
   
 def sendAppointment(subj, description):
   me = 'felll@engr.orst.edu'
-  you = ['latham.fell@base2s.com', 'lathamfell@gmail.com']
-
-  # timezone
+  you = ['latham.fell@base2s.com']
   tz = pytz.timezone("Europe/London")
-  reminderHours = 1
   startHour = 7
-  start = dt.datetime(2015, 1, 16, 16, 30, 0, 0, tz)
+  start = dt.datetime(2015, 1, 17, 16, 30, 0, 0, tz)
   cal = icalendar.Calendar()
   cal.add('prodid', '-//AAAH//engr.orst.edu//')
   cal.add('version', '2.0')
@@ -48,9 +38,9 @@ def sendAppointment(subj, description):
   event.add('description', description)
   event.add('location', "Room 101")
   event.add('dtstart', start)
-  event.add('dtend', dt.datetime(2015, 1, 16, 17, 30, 0, 0, tz))
-  event.add('dtstamp', dt.datetime(2015, 1, 16, 12, 30, 0, 0, tz))
-  event['uid'] = '123456789098765432100000004'
+  event.add('dtend', dt.datetime(2015, 1, 17, 17, 30, 0, 0, tz))
+  event.add('dtstamp', tz.localize(dt.datetime.now()))
+  event['uid'] = 'mcgrath' + str(randomNumber())
   event.add('priority', 5)
   event.add('sequence', 1)
   event.add('created', tz.localize(dt.datetime.now()))
@@ -80,7 +70,6 @@ def sendAppointment(subj, description):
   s.sendmail(msg["From"], [msg["To"]], msg.as_string())
   s.quit()
 
-
 def updateDatabase():
   # db = MySQLdb.connect('oniddb.cws.oregonstate.edu', 
   #                      'felll-db',
@@ -88,6 +77,9 @@ def updateDatabase():
   #                      'felll-db')
   # cur = db.cursor()
   pass
+
+def randomNumber():
+  return random.randrange(1, 1000000+1)
 
 if __name__ == '__main__':
   main()
