95d64ffba1611f2c56427b012443fc4392158229
Latham Fell <lathamfell@gmail.com>
2015-01-25 12:44:13 -0800

    handler sends icalendar with proper datetime

--- handler.py
+++ handler.py
@@ -24,8 +24,62 @@ def main():
   # in production, 'you' will be dmcgrath@eecs.oregonstate.edu
   you = ['lathamfell@gmail.com']
   tz = pytz.timezone('PST8PDT')
-  startHour = 7
-  start = dt.datetime(2015, 1, 30, 16, 30, 0, 0, tz)
+  #target = open('test' + str(random.randrange(1, 998+1)), 'w+')
+  # pull date from email
+  for line in msg.get_payload().split('\n'):
+    # pull appointment date
+    if line.startswith('Date:'):
+      date = line[5:].strip()
+      day = int(date.split(',')[1].split(' ')[2][:-2].strip())
+      year = int(date.split(',')[2].strip())
+      monthString = date.split(',')[1].split(' ')[1].strip()
+      if monthString == 'January':
+        month = 1
+      elif monthString == 'February':
+        month = 2
+      elif monthString == 'March':
+        month = 3
+      elif monthString == 'April':
+        month = 4
+      elif monthString == 'May':
+        month = 5
+      elif monthString == 'June':
+        month = 6
+      elif monthString == 'July':
+        month = 7
+      elif monthString == 'August':
+        month = 8
+      elif monthString == 'September':
+        month = 9
+      elif monthString == 'October':
+        month = 10
+      elif monthString == 'November':
+        month = 11
+      elif monthString == 'December':
+        month = 12
+      else:
+        pass
+    # pull appointment start and end time
+    if line.startswith('Time:'):
+      time = line[5:].strip()
+      # get start time data
+      start = time.split('-')[0].strip()
+      startHour = int(start.split(':')[0].strip())
+      startMinute = int(start.split(':')[1][:2].strip())
+      startCycle = start.split()[1]
+      if startCycle == 'pm':
+        startHour += 12
+      # get end time data
+      end = time.split('-')[1].strip()
+      endHour = int(end.split(':')[0].strip())
+      endMinute = int(end.split(':')[1][:2].strip())
+      endCycle = end.split()[1]
+      if endCycle == 'pm':
+        endHour += 12
+  # save appointment start and end time
+  start = dt.datetime(year, month, day, startHour, startMinute, 0, 0, tz)
+  end = dt.datetime(year, month, day, endHour, endMinute, 0, 0, tz)
+  # build icalendar object
   cal = icalendar.Calendar()
   cal.add('prodid', '-//AAAH//engr.orst.edu//')
   cal.add('version', '2.0')
@@ -40,7 +94,7 @@ def main():
   event.add('description', "Let's get together to celebrate")
   event.add('location', "Room 101")
   event.add('dtstart', start)
-  event.add('dtend', dt.datetime(2015, 1, 30, 17, 30, 0, 0, tz))
+  event.add('dtend', end)
   event.add('dtstamp', tz.localize(dt.datetime.now()))
   event['uid'] = 'mcgrath' + str(random.randrange(1, 999998+1))
   event.add('priority', 5)
