f814044ae4fa9f6f810f3538b3b5a590d9e7f3f2
Latham Fell <lathamfell@gmail.com>
2015-03-17 12:16:15 -0700

    updated formatting

--- handler.py
+++ handler.py
@@ -1,249 +1,38 @@
-import smtplib
-import email
-import time
-import email.MIMEMultipart
-import email.MIMEBase
-import email.MIMEText
-import datetime
-import icalendar
-import pytz
-import random
-import sys
-import socket
-import getpass
-from AAAHDatabase import appointmentExistsSQL, addAppointmentSQL, \
-                         removeAppointmentSQL
+Delivered-To: latham.fell@base2s.com
+Received: by 10.112.19.42 with SMTP id b10csp1364285lbe;
+        Tue, 27 Jan 2015 11:43:19 -0800 (PST)
+X-Received: by 10.66.66.68 with SMTP id d4mr4979170pat.44.1422387797889;
+        Tue, 27 Jan 2015 11:43:17 -0800 (PST)
+Return-Path: <do.not.reply@engr.orst.edu>
+Received: from smtp2.oregonstate.edu (smtp2.oregonstate.edu. [128.193.15.36])
+        by mx.google.com with ESMTP id nu13si2904373pdb.40.2015.01.27.11.43.17
+        for <latham.fell@base2s.com>;
+        Tue, 27 Jan 2015 11:43:17 -0800 (PST)
+Received-SPF: none (google.com: do.not.reply@engr.orst.edu does not designate permitted sender hosts) client-ip=128.193.15.36;
+Authentication-Results: mx.google.com;
+       spf=none (google.com: do.not.reply@engr.orst.edu does not designate permitted sender hosts) smtp.mail=do.not.reply@engr.orst.edu
+Received: from localhost (localhost [127.0.0.1])
+  by smtp2.oregonstate.edu (Postfix) with ESMTP id DBB36380DB;
+  Tue, 27 Jan 2015 11:43:16 -0800 (PST)
+X-Virus-Scanned: amavisd-new at oregonstate.edu
+X-Amavis-Alert: BAD HEADER SECTION, Missing required header field: "Date"
+Received: from smtp2.oregonstate.edu ([127.0.0.1])
+  by localhost (mail.oregonstate.edu [127.0.0.1]) (amavisd-new, port 10024)
+  with ESMTP id fjFR9qf6lg4i; Tue, 27 Jan 2015 11:43:16 -0800 (PST)
+Received: from flip1.engr.oregonstate.edu (flip1.engr.oregonstate.edu [128.193.54.226])
+  by smtp2.oregonstate.edu (Postfix) with ESMTP id C92D8380C8;
+  Tue, 27 Jan 2015 11:43:16 -0800 (PST)
+From: do.not.reply@engr.orst.edu
+To: lathamfell@gmail.com felll@engr.orst.edu
+Subject: Advising Signup with McGrath, D Kevin confirmed for Brabham, Matthew Lawrence
+Message-Id: <20150127194316.DBB36380DB@smtp2.oregonstate.edu>
+Date: Tue, 27 Jan 2015 11:43:16 -0800 (PST)
 
-def main():
-  # set timezone
-  tz = pytz.timezone('PST8PDT')
-  
-  # write to debug log
-  with open("../AAAH/handler_log", 'a') as logfile:
-    logfile.write('\n' + str(tz.localize(datetime.datetime.now())))
-    logfile.write(" : main function called")
-  
-  # bring message in from pipe as an array
-  msg_pipe = sys.stdin.readlines()
-  
-  # join every array element into a single string
-  msg_string = ('').join(msg_pipe)
-  
-  # turn string into message object
-  msg = email.message_from_string(msg_string)
-  
-  # send icalendar invite
-  # socket.getfqdn() issue -- removed
-  user_string = getpass.getuser() + "@engr.orst.edu"
+Advising Signup with McGrath, D Kevin confirmed
+Name: Brabham, Matthew Lawrence
+Email: lathamfell@gmail.com
+Date: Friday, January 30th, 2015
+Time: 3:00pm - 3:30pm
 
-  # For some reason, if multiple emails are entered here, only the first
-  #   email will receive the icalendar invite.  So just use one email here
-  # message_recipiant = ["lathamfell@gmail.com"]
-  # invite should be sent to the same ENGR account
-  message_recipiant = [user_string]
-  
-  # categorize message as signup or cancellation
-  if msg['subject'] == "Advising Signup Cancellation":
-    signup = False
-  else:
-    signup = True
-  
-  # parse email body for data
-  for line in msg.get_payload().split('\n'):
-    
-    # set advisor name and address
-    if line.startswith('Advising Signup with '):
-      advisorName = line[21:].strip()
-      advisorName = advisorName[:-10]
-    
-    # pull student full name. Example: "Brabham, Matthew Lawrence"
-    if line.startswith('Name:'):
-      studentName = line[5:].strip()
-    
-    # pull student email address
-    if line.startswith('Email:'):
-      studentAddress = line[6:].strip()
-    
-    # set advisor email address to this user's email
-    advisorAddress = user_string
-    
-    # pull appointment date
-    if line.startswith('Date:'):
-      dateWithDay = line[5:].strip()
-      day = int(dateWithDay.split(',')[1].split(' ')[2][:-2].strip())
-      year = int(dateWithDay.split(',')[2].strip())
-      monthString = dateWithDay.split(',')[1].split(' ')[1].strip()
-      if monthString == 'January':
-        month = 1
-      elif monthString == 'February':
-        month = 2
-      elif monthString == 'March':
-        month = 3
-      elif monthString == 'April':
-        month = 4
-      elif monthString == 'May':
-        month = 5
-      elif monthString == 'June':
-        month = 6
-      elif monthString == 'July':
-        month = 7
-      elif monthString == 'August':
-        month = 8
-      elif monthString == 'September':
-        month = 9
-      elif monthString == 'October':
-        month = 10
-      elif monthString == 'November':
-        month = 11
-      elif monthString == 'December':
-        month = 12
-      else:
-        pass
-    
-    # pull appointment start and end time
-    if line.startswith('Time:'):
-      time = line[5:].strip()
-      
-      # get start time data
-      startTime12H = time.split('-')[0].strip()
-      startHour = int(startTime12H.split(':')[0].strip())
-      startMinute = int(startTime12H.split(':')[1][:2].strip())
-      startCycle = startTime12H[-2:]
-      if startCycle == 'pm':
-        startHour += 12
-      
-      # get end time data
-      endTime12H = time.split('-')[1].strip()
-      endHour = int(endTime12H.split(':')[0].strip())
-      endMinute = int(endTime12H.split(':')[1][:2].strip())
-      endCycle = endTime12H[-2:]
-      if endCycle == 'pm':
-        endHour += 12
- 
-  # save appointment start and end time
-  startDatetime = datetime.datetime(year, month, day, 
-                  startHour, startMinute, 0, 0, tz)
-  endDatetime = datetime.datetime(year, month, day, 
-                endHour, endMinute, 0, 0, tz)
-  # create uid
-  uid = startDatetime.strftime("%y%m%d%H%M")
-  # check for invalid requests
-  if signup and appointmentExistsSQL(uid):
-    # appointment slot taken. Ignore email
-    
-    # write to log for debugging purposes
-    with open("../AAAH/handler_log", 'a') as logfile:
-      logfile.write('\n' + str(tz.localize(datetime.datetime.now())))
-      logfile.write(" : ignored email for busy appointment slot")
 
-  # elif not signup and not appointmentExists(uid):
-  #   # cancellation for appointment that doesn't exist. Ignore email
-    
-  #   # write to log for debugging purposes
-  #   with open("../AAAH/handler_log", 'a') as logfile:
-  #     logfile.write('\n' + str(tz.localize(datetime.datetime.now())))
-  #     logfile.write(" : ignored cancel email for non-existent appmt")
-
-  else:
-    # request is valid. Process email
-    
-    # build icalendar object
-    cal = icalendar.Calendar()
-    cal.add('prodid', "-//CS419_W2015_Group_2//AAAH 1.0//EN")
-    cal.add('version', "2.0")
-    if signup:
-      body = ""
-      subject = "Advising Appointment for " + studentName
-      cal.add('method', 'REQUEST')
-      cal.add('status', 'confirmed')
-    else:
-      body = "Appointment Cancellation for " + studentName + '\n' + \
-             "When: " + dateWithDay + '\n' + \
-             "Where: Office of " + advisorName
-      subject = "Appointment Cancellation for " + studentName
-      cal.add('method', 'CANCEL')
-      cal.add('status', 'CANCELLED')
-
-    # build the event
-    event = icalendar.Event()
-    for attendee in message_recipiant:
-      event.add('attendee', attendee)
-    event.add('organizer', user_string)
-    event.add('category', "Event")
-    event.add('summary', subject)
-    event.add('description', body)
-    event.add('location', "Office of " + advisorName)
-    event.add('dtstart', startDatetime)
-    event.add('dtend', endDatetime)
-    event.add('dtstamp', tz.localize(datetime.datetime.now()))
-    event['uid'] = uid
-    event.add('priority', 5)
-    if signup:
-      event.add('sequence', 1)
-    else:
-      event.add('sequence', 2)
-    event.add('created', tz.localize(datetime.datetime.now()))
-
-    # add the event to the icalendar object
-    cal.add_component(event)
-
-    # build the outgoing email
-    msg = email.MIMEMultipart.MIMEMultipart('alternative')
-    msg["Subject"] = subject
-    msg["From"] = "do.not.reply@engr.orst.edu"
-    msg["To"] = user_string
-    msg["Date"] = email.utils.formatdate(localtime=True)
-    msg["Content-class"] = "urn:content-classes:calendarmessage"
-    msg.attach(email.MIMEText.MIMEText(body))
-
-    # build the icalendar invite attachment
-    filename = "invite.ics"
-    if signup:
-      part = email.MIMEBase.MIMEBase('text', 'calendar', 
-                                     method='REQUEST', name=filename)
-    else:
-      part = email.MIMEBase.MIMEBase('text', 'calendar',
-                                     method='CANCEL', name=filename)
-    part.set_payload(cal.to_ical())
-    email.Encoders.encode_base64(part)
-    part.add_header('Content-Description', filename)
-    part.add_header('Content-class', "urn:content-classes:calendarmessage")
-    part.add_header("Filename", filename)
-    part.add_header("Path", filename)
-
-    # attach the invite to the outgoing email
-    msg.attach(part)
-
-    # send the email
-    s = smtplib.SMTP("mail.oregonstate.edu")
-    s.sendmail(msg['From'], [msg['To']], msg.as_string())
-    s.quit()
-
-    # update the database
-    if signup:
-      addAppointmentSQL(uid, 
-                     studentName, 
-                     studentAddress,
-                     advisorName,
-                     advisorAddress,
-                     str(startDatetime),
-                     str(endDatetime),
-                     dateWithDay,
-                     startTime12H,
-                     endTime12H)
-
-      # write to log for debugging purposes
-      with open("../AAAH/handler_log", 'a') as logfile:
-        logfile.write('\n' + str(tz.localize(datetime.datetime.now())))
-        logfile.write(" : processed signup email " + uid)
-    else:
-      removeAppointmentSQL(uid)
-
-      # write to log for debugging purposes
-      with open("../AAAH/handler_log", 'a') as logfile:
-        logfile.write('\n' + str(tz.localize(datetime.datetime.now())))
-        logfile.write(" : processed cancel email " + uid)
-
-if __name__ == '__main__':
-  main()
-  
+Please contact support@engr.oregonstate.edu if you experience problems
